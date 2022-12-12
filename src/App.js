298bffc04c4d322dca49b0bc4ebd94ccd2946051
leungpo <polly.leung@oocl.com>
2022-12-12 19:49:15 +0800

    feat: do the todoitem box css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import TodoItem from './features/TodoItem';
 
 function App() {
   return (
     <div className="App">
       <h1>Todo App</h1>
+      <TodoItem/>
     </div>
   );
 }
---
+++ src/features/TodoItem.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import './TodoItem.css';
+export default function TodoItem(props) {
+  return (
+    <div class="box"><p>TodoItem</p></div>
+  )
+}
