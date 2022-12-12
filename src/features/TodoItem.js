39fe06d05a9dda953a19c664f31c19a836d71453
lijo15 <lijo15@oocl.com>
2022-12-12 23:49:15 +0800

    feat: finish todoItem

--- src/features/TodoItem.js
+++ src/features/TodoItem.js
@@ -1,5 +1,17 @@
 import React from "react";
 
-export default function TodoItem() {
-    return <div>TodoItem</div>;
+export default function TodoItem(props) {
+    return (
+        <div
+            style={{
+                width: "400px",
+                border: "1px solid black",
+                textAlign: "center",
+                margin: "10px auto",
+                padding: "5px",
+            }}
+        >
+            {props.item}
+        </div>
+    );
 }
