6233ceb1c682f041a35734174e73d7ccd78a123c
Antony Choi <antony.choi@oocl.com>
2022-12-12 17:59:03 +0800

    feat: add TodoItem components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import React from 'react';
+import TodoItem from './features/TodoItem';
 
 function App() {
   return (
     <div className="App">
       <h1>Todo App</h1>
+      <TodoItem />
     </div>
   );
 }
